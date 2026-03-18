edc3785183b7eab8393370983663a1f907a87fcf
euefrai <bsdfai1001@gmail.com>
2026-03-18 13:21:25 -0300

    corrigindo app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWAInit from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  // Suas outras configurações (images, etc) entram aqui
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
